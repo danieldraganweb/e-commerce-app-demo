190be3926ca8096bb1b391355e081040b27a4201
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-06-12 19:59:51 +0200

    fix syntax error in [id] page.tsx

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import { useState } from "react";
 
@@ -17,12 +15,12 @@ function ProductImage({ product, fill }: Props) {
         <Image
           src={product.image}
           alt={product.title}
-          fill
+          layout="fill"
           className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
             loading
               ? "scale-110 blur-2xl grayscale"
               : "scale-100 blur-0 grayscale-0"
-          }}`}
+          }`}
           onLoadingComplete={() => setLoading(false)}
         />
       ) : (
@@ -35,7 +33,7 @@ function ProductImage({ product, fill }: Props) {
             loading
               ? "scale-110 blur-2xl grayscale"
               : "scale-100 blur-0 grayscale-0"
-          }}`}
+          }`}
           onLoadingComplete={() => setLoading(false)}
         />
       )}
