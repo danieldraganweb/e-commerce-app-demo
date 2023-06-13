6a31863ae43a851fd3f3f67aad0c2bab957c28a5
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-06-13 13:11:54 +0200

    delete not found page and rebuilt [id]

--- app/product/[id]/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="mt-24">
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-    </div>
-  );
-}
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,43 +1,5 @@
-// import Image from "next/image";
-// import { useState } from "react";
+function ProductPage() {
+  return <div>ProductPage</div>;
+}
 
-// type Props = {
-//   product: Product;
-//   fill?: boolean;
-// };
-
-// function ProductImage({ product, fill }: Props) {
-//   const [loading, setLoading] = useState(true);
-
-//   return (
-//     <>
-//       {fill ? (
-//         <Image
-//           src={product.image}
-//           alt={product.title}
-//           width={150}
-//           height={300}
-//           className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
-//             loading
-//               ? "scale-110 blur-2xl grayscale"
-//               : "scale-100 blur-0 grayscale-0"
-//           }`}
-//           onLoadingComplete={() => setLoading(false)}
-//         />
-//       ) : (
-//         <Image
-//           src={product.image}
-//           alt={product.title}
-//           width={400}
-//           height={1000}
-//           className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
-//             loading
-//               ? "scale-110 blur-2xl grayscale"
-//               : "scale-100 blur-0 grayscale-0"
-//           }`}
-//           onLoadingComplete={() => setLoading(false)}
-//         />
-//       )}
-//     </>
-//   );
-// }
+export default ProductPage;
