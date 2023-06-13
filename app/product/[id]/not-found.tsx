9df706923c89dd512cbb9d11c2adbeb8375bbafd
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-06-13 15:32:14 +0200

    create pdp and not-found

--- app/product/[id]/not-found.tsx
+++ app/product/[id]/not-found.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
-const NotFound = () => {
-  return <div>Not found</div>;
-};
-
-export default NotFound;
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">404</h1>
+      <h2 className="text-2xl font-bold">Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
