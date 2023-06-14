893e672614afa1032b072912c2f1770473145f24
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-06-14 11:49:50 +0200

    style footer max width 80rem/* 1280px */

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="max-w-6xl mx-auto py-4 bg-gray-100 text-white-800 flex flex-wrap justify-center">
+    <footer className="max-w-7xl mx-auto py-4 bg-gray-100 text-white-800 flex flex-wrap justify-center">
       <div className="flex flex-col">
         <ul className="flex flex-wrap">
           <li className="px-4 py-2 text-gray-500">
